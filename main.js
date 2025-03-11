6cbaf49c746eec21a8b2af5806d645e7601edaf1
itspitso <147466450+itspitso@users.noreply.github.com>
2025-03-11 08:12:57 +0200

    Added try catch to catch zero division errors

--- main.js
+++ main.js
@@ -11,9 +11,34 @@ const multiply = function(num1, num2) {
 }
 
 const divide = function(num1, num2) {
-    return num1 / num2;
+    try {
+        return num1 / num2;
+    }
+    catch(e) {
+        "Nope. Can't do that";
+    }
 }
 
 let firstNumber;
 let operator;
 let secondNumber;
+
+const operate = function(first, op, second) {
+    switch (op) {
+        case '+':
+            add(first, second);
+            break;
+        case '-':
+            subtract(first, second);
+            break;
+        case '*':
+            multiply(first, second);
+            break;
+        case '/':
+            divide(first, second);
+            break;
+        default:
+            throw new Error("Operator not recognised");
+    }
+
+}
