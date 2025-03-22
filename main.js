c5815ef1d81757548752e49a914a1092a3c80dbb
itspitso <147466450+itspitso@users.noreply.github.com>
2025-03-22 21:44:08 +0200

    Made changes to the layout and started with accessing the DOM

--- main.js
+++ main.js
@@ -11,19 +11,20 @@ const multiply = function(num1, num2) {
 }
 
 const divide = function(num1, num2) {
-    try {
-        return num1 / num2;
+    if (num2 === 0) {
+        screen.innerText = "Nope! Can't do that."
     }
-    catch(e) {
-        "Nope. Can't do that";
-    }
+    return num1 / num2;
 }
 
 let firstNumber;
 let operator;
 let secondNumber;
+let expression;
+let result;
+const opRegex = /+|*|-|\//;
 
-function operate(first, op, second) {
+const operate = function(first, op, second) {
     switch (op) {
         case '+':
             return add(first, second);
@@ -40,5 +41,25 @@ function operate(first, op, second) {
         default:
             throw new Error("Operator not recognised");
     }
-
 }
+
+const screen = document.querySelector(".screen");
+const clearButton = document.querySelector(".clear").onclick(reset);
+const negateButton = document.querySelector(".negate");
+const percentageButton = document.querySelector(".percentage");
+const divideButton = document.querySelector(".divide");
+const sevenButton = document.querySelector(".seven");
+const eightButton = document.querySelector(".eight");
+const nineButton = document.querySelector(".nine");
+const multiplyButton = document.querySelector(".multiply");
+const fourButton = document.querySelector(".four");
+const fiveButton = document.querySelector(".five");
+const sixButton = document.querySelector(".six");
+const minusButton = document.querySelector(".minus")
+const oneButton = document.querySelector(".one");
+const twoButton = document.querySelector(".two");
+const threeButton = document.querySelector(".three");
+const plusButton = document.querySelector(".plus");
+const periodButton = document.querySelector(".period")
+const zeroButton = document.querySelector(".zero");
+const equalButton = document.querySelector(".equal");
